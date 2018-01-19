3adc1475acf69230ddd1af30bc71ab79d573b6db
Adarsh-MR <adarshmr1998@gmail.com>
2018-01-19 16:11:54 +0000

    hlte-common: wifi: Add back hostapd configs
    * CAF softap implementation requires this
    
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- hlte.mk
+++ hlte.mk
@@ -254,7 +254,10 @@ PRODUCT_PACKAGES += \
 # WIFI Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/hostapd.accept:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/wifi/hostapd.deny:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.deny \
+    $(LOCAL_PATH)/wifi/hostapd_default.conf:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd_default.conf
 
 # WIFI HAL
 PRODUCT_PACKAGES += \
