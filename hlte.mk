6722946fc91af54f4a0e57157a59edeb63af68bc
Bruno Martins <bgcngm@gmail.com>
2018-01-26 11:27:35 +0000

    hlte-common: Binderize them all
     * Switch to binderized HAL services as possible and update
       HIDL manifest accordingly.
    
     * Following considerations regarding those without any change:
    
       - Sensors left out of the game because wouldn't work
         otherwise.
    
    Change-Id: Id50291488d655187aa013c51bdd6890dca010564

--- hlte.mk
+++ hlte.mk
@@ -51,6 +51,7 @@ PRODUCT_COPY_FILES += \
 # Audio HAL
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     audio_policy.msm8974 \
     audio.a2dp.default \
@@ -66,6 +67,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Camera
@@ -87,6 +89,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
@@ -136,7 +139,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Keystore
 PRODUCT_PACKAGES += \
