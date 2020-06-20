fa2029de56618ccfa1c2dc8a4e0f021d6cb2e1c6
Jprimero15 <jprimero155@gmail.com>
2020-06-20 06:36:32 +0000

    hlte-common: Build SamsungDoze
    * This also reverts commit dd551d392666fd47766a4d4b91eb9e79f97afd6d.
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>
    Change-Id: I439f40c8c3c9ec52037eef7b22055fe6cb348384

--- hlte.mk
+++ hlte.mk
@@ -193,10 +193,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# ParanoidDoze
-PRODUCT_PACKAGES += \
-    ParanoidDoze
-
 # Power HAL
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service
@@ -216,6 +212,10 @@ PRODUCT_PACKAGES += \
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
+
+# SamsungDoze
+PRODUCT_PACKAGES += \
+    SamsungDoze
 
 # Sensors
 PRODUCT_COPY_FILES += \
